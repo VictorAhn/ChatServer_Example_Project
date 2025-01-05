e57512c0091e35318552e4b9b5ee1e57c5ccb2e8
현수 안 <sj99119@naver.com>
2025-01-06 00:37:31 +0900

    Swagger Endpoint 수정

--- ChatService/main.py
+++ ChatService/main.py
@@ -2,11 +2,11 @@ from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
 from chat.routes import router as chat_router
 
-app = FastAPI(title="Chat Service API", description="MSA 구성 예제 구성", version="1.0.0")
+app = FastAPI(title="Chat Service API", description="MSA 구성 예제 구성", version="1.0.0", docs_url="/chat/docs")
 
 # Register routers
 app.include_router(chat_router, prefix="/chat", tags=["Chat"])
 
 @app.get("/", include_in_schema=False)
 def redirect_to_docs():
-    return RedirectResponse(url='/docs')
+    return RedirectResponse(url='/chat/docs')
--- UserService/main.py
+++ UserService/main.py
@@ -3,7 +3,7 @@ from fastapi.responses import RedirectResponse
 from auth.routes import router as auth_router
 from friends.routes import router as friends_router
 
-app = FastAPI(title="User Service API", description="MSA 구성 예제 구성", version="1.0.0")
+app = FastAPI(title="User Service API", description="MSA 구성 예제 구성", version="1.0.0", docs_url="/user/docs")
 
 # Register routers
 app.include_router(auth_router, prefix="/auth", tags=["Auth"])
@@ -11,4 +11,4 @@ app.include_router(friends_router, prefix="/friends", tags=["Friends"])
 
 @app.get("/", include_in_schema=False)
 def redirect_to_docs():
-    return RedirectResponse(url='/docs')
+    return RedirectResponse(url='/user/docs')
